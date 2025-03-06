0470a9d74cd513c2cfab4d7407cd6e0c50242383
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2025-03-06 13:11:33 +0100

    Unit tests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -2,7 +2,27 @@
 //  ArrayTests.swift
 //  MovieQuizTests
 //
-//  Created by Sofya Tarnalitskaya on 04/03/2025.
+//  Created by Sofya Tarnalitskaya on 06/03/2025.
 //
 
-import Foundation
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+        let array = [1, 1, 2, 3, 5]
+        
+        let value = array[safe: 2]
+        
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        let array = [1, 1, 2, 3, 5]
+        
+        let value = array[safe: 20]
+        
+        XCTAssertNil(value)
+    }
+}
